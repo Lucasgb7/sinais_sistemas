32d3d1e53dca929aa46be18f45bd4a498659bd90
Lucas J. Cunha <lucasgb7_ks@hotmail.com>
2020-09-15 08:46:39 -0300

    inserindo valores no x

--- Processamento Digitais de Sinais/Aula_03/convolucao.py
+++ Processamento Digitais de Sinais/Aula_03/convolucao.py
@@ -8,9 +8,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    x = np.full(6, 1)   # vetor 1x6, apenas com 1
-    h = [1, 0.5, 0.25, 0.125]
+    v = [np.full(8, 1), [1, 0, 0, 0, 0, 0, 0, 0], [1, 0.5, 0.25, 0.125]]
+    x = 0
+    while x is 0:
+        print("0-Degrau Unitario \n1-Impulso Unitario \n2-x[n]=[1, 0.5, 0.25, 0.125]")
+        a = int(input("Selecione uma das opções: "))
+        if 0 <= a <= 2:
+            x = v[a]
     
+    h = [1, 0.5, 0.25, 0.125]
     y = np.convolve(x, h)   # retorna a convolucao linear e discreta dos dois vetores
     
     plt.grid(True)
