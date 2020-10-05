ada409204e739bffc0c183cc205427c48a85f98b
Lucas J. Cunha <lucasgb7_ks@hotmail.com>
2020-10-05 15:58:55 -0300

    transformada fourier pronta

--- Processamento Digitais de Sinais/Aula_04/tfd.py
+++ Processamento Digitais de Sinais/Aula_04/tfd.py
@@ -11,20 +11,20 @@ a = 0.5
 w = np.arange(-1*np.pi, 1*np.pi, np.pi/100) # -pi : pi/100 : pi
 
 num = 1
-den = 1 - a**(complex(0, -1)*w) # 1-a^(-j*w) 
+den = 1 - a * np.exp(-1j*w) # (complex(0, -1)*w) # 1-a^(-j*w) 
 x = num/den
 
 mod_x = np.absolute(x)
 fase_x = np.angle(den)
 
-plt.subplot(221)
-plt.show(w, mod_x)
+plt.plot(mod_x)
 plt.title('Modulo de X')
 plt.xlabel('w')
 plt.grid(True)
 
-plt.subplot(222)
-plt.show(w, fase_x)
+plt.plot(fase_x)
 plt.title('Fase de X')
 plt.xlabel('w')
 plt.grid(True)
+
+plt.show()
