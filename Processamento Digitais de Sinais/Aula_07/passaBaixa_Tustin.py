cbe576badb988be958ec7d3f72a3f7361c675f27
Lucas J. Cunha <lucasgb7_ks@hotmail.com>
2020-10-13 14:59:29 -0300

    transferFunction error

--- Processamento Digitais de Sinais/Aula_07/passaBaixa_Tustin.py
+++ Processamento Digitais de Sinais/Aula_07/passaBaixa_Tustin.py
@@ -11,32 +11,23 @@ H(s) = -----------
 S -> Z
 H[Z] = Y[Z] / X[Z]
 """
-# Definindo a especificação do filtro
-
-# R = 10;
-# C = 10*10^-6;
-# W0 = 1/(R*C)
 W0 = 2*pi*1000
-
-f0 = W0/(2*pi)  # frequencia de sintonia em Hz
+f0 = W0/(2*pi)  # F0 em HZ
 
 # w0 = 2*pi*f0;
 
-# Definindo os coeficientes em s
-
+# Definindo os coeficientes
 num = [W0, 0]
 den = [W0, 1]
-
 H = TransferFunction(num, den)
 print(type(H))
 bode(H)
 
-# Aplicando a transf Biliner S->Z
-Fs = 8000   # Frequência de amostragem
+Fs = 8000
 Ts = 1/Fs
 
 # Transforma de continuo para discreto, utilizando metodo de Tustin
-Hd = cont2discrete(H, Ts, method='bilinear')
+Hd = cont2discrete(H, Ts, 'bilinear')
 
 # Plotar em frequencia (Hz)
 [H, w] = freqz(Hd.Numerator[0, 0], Hd.Denominator[0, 0], fs=Fs/(2*pi))
